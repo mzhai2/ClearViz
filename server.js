1991a842d951e5ff856ffc6ef435b5b7d9d1e367
Michael Zhai <mzhai2@emory.edu>
2015-03-04 21:24:17 -0500

    Edited index page

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
-var express = require('express'),
-	app = express();
+var express = require('express')
+  , passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy
+  , mongodb = require('mongodb')
+  , mongoose = require('mongoose')
+  , bcrypt = require('bcrypt')
+  , SALT_WORK_FACTOR = 10;
+
+var app = express();
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
 
 app.get('/', function(req, res) {
-	res.sendfile(__dirname + '/client/views/index.html');
+	res.sendFile(__dirname + '/client/views/index.html');
 });
 
 app.listen(3000, function() {
 	console.log('I\'m Listening...');
 })
+
+// app.post('/login',
+//   passport.authenticate('local', { successRedirect: '/',
+//                                    failureRedirect: '/login'})
+// );
+
+app.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/users/' + req.user.username);
+  });
+
+
