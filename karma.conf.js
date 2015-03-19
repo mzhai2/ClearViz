8a8ef29576573718ef412982d2af9408467236e0
Deh Jun Tzou <dtzou3@emory.edu>
2015-03-19 02:53:16 -0400

    edited karma config file
    changed lib to component. not sure how to change the last three files..

--- karma.conf.js
+++ karma.conf.js
@@ -5,9 +5,16 @@ module.exports = function(config)
   config.set({
     frameworks: ['jasmine'],
     files: [
-'public/lib/angular/angular.js', 'public/lib/angular-resource/angular-resource.js', 'public/lib/angular-route/angular-route.js', 'public/lib/angular-mocks/angular-mocks.js', 'public/application.js',
-'public/*[!lib]*/*.js', 'public/*[!lib]*/*[!tests]*/*.js', 'public/*[!lib]*/tests/unit/*.js'
-],
+        'public/components/angular/angular.js',
+        'public/components/angular-resource/angular-resource.js',
+        'public/components/angular-route/angular-route.js',
+        'public/components/angular-mocks/angular-mocks.js',
+        'public/application.js',
+        'public/*[!lib]*/*.js',
+        'public/*[!lib]*/*[!tests]*/*.js',
+        'public/*[!lib]*/tests/unit/*.js'
+    ],
+
   reporters: ['progress'],
   browsers: ['PhantomJS','Chrome','Safari'],
   captureTimeout: 60000,
