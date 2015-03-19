a969e66586120058e05996e8b836b0c503b19b22
Deh Jun Tzou <dtzou3@emory.edu>
2015-03-19 01:49:32 -0400

    unit test for trees module

---
+++ public/trees/tests/unit/trees.client.module.unit.tests.js
@@ -0,0 +1,10 @@
+describe('Testing MEAN Main Module', function() { var mainModule;
+
+    beforeEach(function() {
+    mainModule = angular.module('trees');
+    });
+
+    it('Should be registered', function() { expect(mainModule).toBeDefined();
+    });
+
+});
