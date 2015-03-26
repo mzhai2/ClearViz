412aebf6adcc8427ad49bfe7d3073cb818dbe120
Michael Zhai <mzhai2@emory.edu>
2015-03-26 04:04:42 -0400

    need to use preprocessor?

--- karma.conf.js
+++ karma.conf.js
@@ -13,8 +13,15 @@ module.exports = function(config)
         'public/js/jquery-1.11.2.min.js',
         'public/*[!lib]*/*.js',
         'public/*[!lib]*/*[!tests]*/*.js',
-        'public/*[!lib]*/tests/unit/*.js',
+        'public/*[!lib]*/tests/unit/*.js'
     ],
+    // preprocessors: {
+    //     'public/application.js': ['ng-html2js'],
+    //     'public/*[!lib]*/tests/unit/*.js': ['ng-html2js'],
+    //     'public/components/angular-resource/angular-resource.js': ['ng-html2js'],
+    //     'public/components/angular-route/angular-route.js': ['ng-html2js'],
+    //     'public/components/angular-mocks/angular-mocks.js': ['ng-html2js'],
+    // },
 
     reporters: ['progress'],
     browsers: ['PhantomJS','Chrome','Safari'],
