350c395ade8e10dac186900c059dc821c164ea32
ateng3 <ateng3@emory.edu>
2015-04-13 23:25:28 -0400

    edit tree modal fix

--- public/trees/controllers/trees.client.controller.js
+++ public/trees/controllers/trees.client.controller.js
@@ -36,6 +36,7 @@ angular.module('trees').controller('TreesController', ['$scope', '$rootScope', '
             function(errorResponse) {
                 $scope.error = errorResponse.data.message;
             });
+        $('#edit').modal('hide');
     };
 
     $scope.delete = function(tree) {
@@ -104,8 +105,15 @@ angular.module('trees').controller('TreesController', ['$scope', '$rootScope', '
 
         $('#create').modal({show:false});
         $('body').removeClass('modal-open');
-        $('.modal-backdrop').removeClass("modal-backdrop");
-    }]);
+        $('.modal-backdrop').removeClass("modal-backdrop");        
+        }]);
+
+        
+        
+
+        
+
+
 
 Array.prototype.clean = function(deleteValue) {
     for (var i = 0; i < this.length; i++) {
