1c196918dd51bfcbf2580cb0280bdaaa4c8af35a
Michael Zhai <mzhai2@emory.edu>
2015-04-14 21:39:16 -0400

    removed unnecessary promise

--- public/trees/controllers/trees.client.controller.js
+++ public/trees/controllers/trees.client.controller.js
@@ -66,68 +66,59 @@ angular.module('trees').controller('TreesController', ['$scope', '$rootScope', '
                 treeData[treeData.length] = ["NEWSENTENCE"]; // adds spacer
             }
         });
-        var deferred = $q.defer();
-        var promise = deferred.promise;
-        promise.then(function (treeData) {
-            function getNext(treeData) {
-                getNext.count = ++getNext.count || 0;
-                if (treeData[getNext.count][0] === "NEWSENTENCE")
-                    getNext.count++;
-                return getNext.count;
-            }
-            var i,k;
-            var words, word;
-            for (i=0; i < childNodes.length; i++) {
-                var node = childNodes[i];
-                if (node.nodeType == 3) {
-                    words = node.nodeValue.split(" ").clean("");
-                    for (k=0; k<words.length; k++) {
-                        if (words[k]){
-                            treeData[getNext(treeData)][7] = "O";
-                        }
 
+        function getNext(treeData) {
+            getNext.count = ++getNext.count || 0;
+            if (treeData[getNext.count][0] === "NEWSENTENCE")
+                getNext.count++;
+            return getNext.count;
+        }
+        var i,k;
+        var words, word;
+        for (i=0; i < childNodes.length; i++) {
+            var node = childNodes[i];
+            if (node.nodeType == 3) {
+                words = node.nodeValue.split(" ").clean("");
+                for (k=0; k<words.length; k++) {
+                    if (words[k]){
+                        treeData[getNext(treeData)][7] = "O";
                     }
-                }
-                if (node.nodeType == 1) {
-                    var name = node.className.substring(0,3).toUpperCase();
-                    words = node.innerHTML.split(" ").clean("");
-                    if (words.length == 1)
-                        treeData[getNext(treeData)][7] = "U-" + name;
-                    else {
-                        treeData[getNext(treeData)][7] = "B-" + name;
-                        for (k=1; k<words.length-1; k++) {
-                            treeData[getNext(treeData)][7] = "I-" + name;
-                        }
-                        treeData[getNext(treeData)][7] = "L-" + name;
-                    }
+
                 }
             }
-            console.log(treeData);
-            return treeData;
-        })
-        .then(function (treeData) {
-            var tsv = "";
-            for (i=0;i<treeData.length;i++) {
-                if (treeData[i][0] === "NEWSENTENCE")
-                    tsv+="\n";
+            if (node.nodeType == 1) {
+                var name = node.className.substring(0,3).toUpperCase();
+                words = node.innerHTML.split(" ").clean("");
+                if (words.length == 1)
+                    treeData[getNext(treeData)][7] = "U-" + name;
                 else {
-                    tsv+=treeData[i][0]+"\t"+treeData[i][1]+"\t"+treeData[i][2]+"\t"+treeData[i][3]+"\t"+treeData[i][4]+"\t"+treeData[i][5]+"\t"+treeData[i][6]+"\t"+treeData[i][7]+"\n";
+                    treeData[getNext(treeData)][7] = "B-" + name;
+                    for (k=1; k<words.length-1; k++) {
+                        treeData[getNext(treeData)][7] = "I-" + name;
+                    }
+                    treeData[getNext(treeData)][7] = "L-" + name;
                 }
             }
-            var req = new Annotations($scope.tree);
-            req.data=tsv;
-            return req;
-        })
-        .then(function (req) {
-            req.$annotate(function(response) {
-                $window.alert("saved");
-            },
-            function(errorResponse) {
-                $scope.error = errorResponse;
-                $window.alert(errorResponse);
-            });
+        }
+        var tsv = "";
+        for (i=0;i<treeData.length;i++) {
+            if (treeData[i][0] === "NEWSENTENCE")
+                tsv+="\n";
+            else {
+                tsv+=treeData[i][0]+"\t"+treeData[i][1]+"\t"+treeData[i][2]+"\t"+treeData[i][3]+"\t"+treeData[i][4]+"\t"+treeData[i][5]+"\t"+treeData[i][6]+"\t"+treeData[i][7]+"\n";
+            }
+        }
+        var req = new Annotations($scope.tree);
+        req.data=tsv;
+        
+        req.$annotate(function(response) {
+            $window.alert("saved");
+        },
+        function(errorResponse) {
+            $scope.error = errorResponse;
+            $window.alert(errorResponse);
         });
-        deferred.resolve(treeData);
+        
     };
     $rootScope.$on('keypress', function (evt, obj, key) {
         if (key == 'z')
@@ -143,9 +134,9 @@ angular.module('trees').controller('TreesController', ['$scope', '$rootScope', '
         }
     });
 
-$('#create').modal({show:false});
-$('body').removeClass('modal-open');
-$('.modal-backdrop').removeClass("modal-backdrop");
+    $('#create').modal({show:false});
+    $('body').removeClass('modal-open');
+    $('.modal-backdrop').removeClass("modal-backdrop");
 }]);
 
 Array.prototype.clean = function(deleteValue) {
