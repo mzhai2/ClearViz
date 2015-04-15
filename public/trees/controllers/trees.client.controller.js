88456a98486f4b5f156cacea7451e79dd0d243b6
Michael Zhai <mzhai2@emory.edu>
2015-04-14 21:35:10 -0400

    fixed async for loop

--- public/trees/controllers/trees.client.controller.js
+++ public/trees/controllers/trees.client.controller.js
@@ -1,4 +1,4 @@
-angular.module('trees').controller('TreesController', ['$scope', '$rootScope', '$routeParams', '$location', '$cookieStore', '$window', 'Trees', 'Annotations', 'annotationFactory', function($scope, $rootScope, $routeParams, $location, $cookieStore, $window, Trees, Annotations, annotationFactory) {
+angular.module('trees').controller('TreesController', ['$scope', '$rootScope', '$routeParams', '$location', '$cookieStore', '$window', '$q', 'Trees', 'Annotations', 'annotationFactory', function($scope, $rootScope, $routeParams, $location, $cookieStore, $window, $q, Trees, Annotations, annotationFactory) {
     $rootScope.loggedIn = $cookieStore.get('loggedin');
     $scope.create = function() {
         var tree = new Trees({
@@ -57,63 +57,77 @@ angular.module('trees').controller('TreesController', ['$scope', '$rootScope', '
     $scope.annotateNer = function() {
         var div = document.getElementById('annotation');
         var childNodes = div.childNodes[1].childNodes;
-        var treeData = new Array();
+        var treeData = [];
         var data = $scope.tree.data;
         d3.tsv.parseRows(data, function(data) {
             if(data[1])
                 treeData[treeData.length] = data;
             else {
-                // treeData[treeData.length] = ["NEWSENTENCE"]; // adds spacer
+                treeData[treeData.length] = ["NEWSENTENCE"]; // adds spacer
             }
         });
-        console.log(treeData)
-        var i,j=0,k;
-        var words, word;
-        for (i=0; i < childNodes.length; i++) {
-            if (treeData[j][0] == "NEWSENTENCE")
-                j++;
-            var node = childNodes[i];
-
-            if (node.nodeType == 3) {
-                words = node.nodeValue.split(" ").clean("");
-                for (k=0; k<words.length; k++) {
-                    if (words[k])
-                        treeData[j++][7] = "O";
-                }
+        var deferred = $q.defer();
+        var promise = deferred.promise;
+        promise.then(function (treeData) {
+            function getNext(treeData) {
+                getNext.count = ++getNext.count || 0;
+                if (treeData[getNext.count][0] === "NEWSENTENCE")
+                    getNext.count++;
+                return getNext.count;
             }
-            if (node.nodeType == 1) {
-                var name = node.className.substring(0,3).toUpperCase();
-                words = node.innerHTML.split(" ").clean("");
-                if (words.length == 1)
-                    treeData[j++][7] = "U-" + name;
-                else {
-                    treeData[j++][7] = "B-" + name;
-                    for (k=1; k<words.length-1; k++) {
-                        treeData[j++][7] = "I-" + name;
+            var i,k;
+            var words, word;
+            for (i=0; i < childNodes.length; i++) {
+                var node = childNodes[i];
+                if (node.nodeType == 3) {
+                    words = node.nodeValue.split(" ").clean("");
+                    for (k=0; k<words.length; k++) {
+                        if (words[k]){
+                            treeData[getNext(treeData)][7] = "O";
+                        }
+
                     }
-                    treeData[j++][7] = "L-" + name;
+                }
+                if (node.nodeType == 1) {
+                    var name = node.className.substring(0,3).toUpperCase();
+                    words = node.innerHTML.split(" ").clean("");
+                    if (words.length == 1)
+                        treeData[getNext(treeData)][7] = "U-" + name;
+                    else {
+                        treeData[getNext(treeData)][7] = "B-" + name;
+                        for (k=1; k<words.length-1; k++) {
+                            treeData[getNext(treeData)][7] = "I-" + name;
+                        }
+                        treeData[getNext(treeData)][7] = "L-" + name;
+                    }
                 }
             }
-        }
-        var tsv = "";
-        for (i=0;i<treeData.length;i++) 
-            if (treeData[i][0] == "NEWSENTENCE")
-                tsv+="\n";
-            else {
-                tsv+=treeData[i][0]+"\t"+treeData[i][1]+"\t"+treeData[i][2]+"\t"+treeData[i][3]+"\t"+treeData[i][4]+"\t"+treeData[i][5]+"\t"+treeData[i][6]+"\t"+treeData[i][7]+"\n";
+            console.log(treeData);
+            return treeData;
+        })
+        .then(function (treeData) {
+            var tsv = "";
+            for (i=0;i<treeData.length;i++) {
+                if (treeData[i][0] === "NEWSENTENCE")
+                    tsv+="\n";
+                else {
+                    tsv+=treeData[i][0]+"\t"+treeData[i][1]+"\t"+treeData[i][2]+"\t"+treeData[i][3]+"\t"+treeData[i][4]+"\t"+treeData[i][5]+"\t"+treeData[i][6]+"\t"+treeData[i][7]+"\n";
+                }
             }
-        var req = new Annotations($scope.tree);
-        req.data=tsv;
-        console.log(tsv);
-        // console.log(data);
-
-        req.$annotate(function(response) {
-            $window.alert("saved");
-        },
-        function(errorResponse) {
-            $scope.error = errorResponse.data.message;
-            $window.alert(errorResponse.data.message);
+            var req = new Annotations($scope.tree);
+            req.data=tsv;
+            return req;
+        })
+        .then(function (req) {
+            req.$annotate(function(response) {
+                $window.alert("saved");
+            },
+            function(errorResponse) {
+                $scope.error = errorResponse;
+                $window.alert(errorResponse);
+            });
         });
+        deferred.resolve(treeData);
     };
     $rootScope.$on('keypress', function (evt, obj, key) {
         if (key == 'z')
